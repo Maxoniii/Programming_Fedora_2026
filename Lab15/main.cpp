fadca25ed39d03f9948678d2da026be6c000b30d
Maxoniii <maxkirilenkobest@gmail.com>
2026-01-17 11:59:34 +0200

    Obnova

--- Lab15/main.cpp
+++ Lab15/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-
+#include "Spfere.hpp"
 int main() {
-    std::cout << "Update "<< std::endl;
+    try{
+        Spfere spfere(5.0f);
+        std::cout<<"Spfere radius"<<spfere.getRad()<<std::endl;
+        
+    }
+    catch(const std::invalid_argument& ex){
+        std::cerr << ex.what()<<std::endl;
+    }
     return 0;
 }
+
